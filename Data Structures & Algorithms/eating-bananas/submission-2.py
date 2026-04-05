f052673284202d48c0fa1ef34ea3f1d51439dd7a
Akanksha Agrawal <aakankshaa2606@gmail.com>
2026-04-05 12:53:35 -0500

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        low = 1
+        high = max(piles)
+        ans = high
+        n = len(piles)
+        if h == n:
+            return high
+
+        def canDo(k):
+            cnt = 0
+            for i in range(n):
+                x = piles[i]
+                # cnt += math.ceil(x/k)
+                cnt += (x + k - 1) // k
+                if cnt > h:
+                    return False
+            return cnt <= h
+
+        while low<=high:
+
+            mid = (low+high)//2
+
+            if canDo(mid):
+                ans = min(ans,mid)
+                high = mid-1 #try at a lower speed
+            else:
+                low = mid+1 # increase the speed
+
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
